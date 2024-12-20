5b834a78d3a62ab22f736eb92f60f3e3043f8179
josuemorais <josuemorais@gmail.com>
2024-12-20 13:13:46 -0300

    refactor jqueue and jtask

--- include/util/jqueue.h
+++ include/util/jqueue.h
@@ -1,5 +1,5 @@
 #ifndef __JQUEUE_H
-#define __TQUEUE_H
+#define __JQUEUE_H
 
 #include <Arduino.h>
 #include <stdlib.h>
@@ -9,21 +9,18 @@
 #define MAXLENGTHJQUEUE 5
 #endif
 
-// Estrutura da fila
 typedef struct {
-    void *buffer[MAXLENGTHJQUEUE]; // Ponteiro para o buffer da fila
-    uint8_t head;        // Índice do próximo item a ser lido
-    uint8_t tail;        // Índice do próximo espaço para escrita
-    uint8_t count;       // Número atual de itens na fila
+    void (*buffer[MAXLENGTHJQUEUE])(); // Ponteiro para o buffer da fila
+    uint8_t head; // Índice do próximo item a ser lido
+    uint8_t tail; // Índice do próximo espaço para escrita
+    uint8_t count; // Número atual de itens na fila
 } jQueue_t;
 
 typedef jQueue_t* jQueueHandle_t;
 
 // Função para criar uma fila
 bool jQueueCreate(jQueueHandle_t queue) {
-    if (queue == nullptr) {
-        return false;         // Fila cheia
-    }
+    if (queue == nullptr) return false;
     queue->head = 0;
     queue->tail = 0;
     queue->count = 0;
@@ -31,36 +28,24 @@ bool jQueueCreate(jQueueHandle_t queue) {
 }
 
 // Função para enviar um item para a fila a partir de uma ISR
-bool jQueueSendFromISR(jQueueHandle_t xQueue,void *pvItemToQueue) {
-    if (xQueue->count == MAXLENGTHJQUEUE) {
-        return false;         // Fila cheia
-    }
-
-    // Desabilita interrupções para seção crítica
+bool jQueueSendFromISR(jQueueHandle_t xQueue, void (*pvItemToQueue)()) {
+    if (xQueue->count == MAXLENGTHJQUEUE) return false;
     noInterrupts();
-    xQueue->tail = (xQueue->tail + 1) % MAXLENGTHJQUEUE;
     xQueue->buffer[xQueue->tail] = pvItemToQueue;
-    xQueue->count++;    
-    // Habilita interrupções após seção crítica
+    xQueue->tail = (xQueue->tail + 1) % MAXLENGTHJQUEUE;
+    xQueue->count++;
     interrupts();
-
     return true;
 }
 
 // Função para receber um item da fila
-bool jQueueReceive(jQueueHandle_t xQueue, void *pvBuffer) {
-    if (xQueue->count == 0) {
-        return false;         // Fila vazia
-    }
-
-    // Desabilita interrupções para seção crítica
-    noInterrupts();
-    xQueue->head = (xQueue->head + 1) % MAXLENGTHJQUEUE;
-    pvBuffer = xQueue->buffer[xQueue->head];
-    xQueue->count--;
-    // Habilita interrupções após seção crítica
-    interrupts();
-
+bool jQueueReceive(jQueueHandle_t xQueue, void (**pvBuffer)()) {
+    if (xQueue->count == 0) return false; // Retorna false se a fila estiver vazia
+    noInterrupts(); // Desativa interrupções para acesso seguro
+    *pvBuffer = xQueue->buffer[xQueue->head]; // Atribui o item ao ponteiro fornecido
+    xQueue->head = (xQueue->head + 1) % MAXLENGTHJQUEUE; // Incrementa o índice de leitura
+    xQueue->count--; // Decrementa o contador de itens
+    interrupts(); // Habilita interrupções novamente
     return true;
 }
 
--- include/util/jtask.h
+++ include/util/jtask.h
@@ -1,93 +1,70 @@
-#include "Arduino.h" // Biblioteca base do framework Arduino, necessária para funções básicas como Serial e delays.
-#include "util/jqueue.h" // Inclusão de uma biblioteca para gerenciamento de filas.
+#include "Arduino.h"
+#include "util/jqueue.h"
 
-uint8_t jtaskIndex = 0; // Índice usado para gerenciar as tarefas adicionadas à estrutura de tarefas.
+uint8_t jtaskIndex = 0;
 
 #ifndef NUMTASKS
-#define NUMTASKS 2 // Define o número máximo de tarefas se não estiver definido anteriormente.
+#define NUMTASKS 2
 #endif
 
-#ifdef ESP32
-    // Configuração específica para ESP32
-    // Referência: https://docs.espressif.com/projects/arduino-esp32/en/latest/api/timer.html
-    hw_timer_t *timer = nullptr; // Ponteiro global para o timer de hardware do ESP32.
-#else
-    // Configuração específica para Arduino Uno sem a biblioteca TimerOne
-#endif
-
-// Estrutura para armazenar informações de contadores e limites
-struct CounterConfig_t
-{
-    uint16_t counter; // Contador atual.
-    uint16_t limit;   // Limite que, ao ser atingido, dispara a tarefa.
-    void (*task)();   // Ponteiro para a função que será executada.
+// Estrutura para configuração das tarefas
+struct CounterConfig_t {
+    uint16_t counter;
+    uint16_t limit;
+    void (*task)();
 };
 
-// Array para armazenar a configuração de tarefas
-CounterConfig_t jtaskStruct[NUMTASKS]; // Array de tarefas com tamanho definido por NUMTASKS.
+CounterConfig_t jtaskStruct[NUMTASKS]; // Array de tarefas
+jQueue_t jtaskQueue; // Fila para execução das tarefas
 
-jQueue_t jtaskQueue; // Fila para armazenar as tarefas pendentes a serem executadas.
-
-// Função de callback chamada pelo timer
 #ifdef ESP32
-    void IRAM_ATTR timerCallback() // No ESP32, essa função precisa ser executada na IRAM (memória de interrupção).
+hw_timer_t *timer = nullptr;
+void IRAM_ATTR timerCallback() { // Função de callback do timer no ESP32
 #else
-    ISR(TIMER1_COMPA_vect) // No Arduino Uno, a função é chamada diretamente pela interrupção do timer sem TimerOne.
+ISR(TIMER2_COMPA_vect) { // Função de callback do timer no Arduino Uno (Timer 2)
 #endif    
-{
-    // Itera sobre todas as tarefas configuradas
-    for (CounterConfig_t *c = jtaskStruct; c->task != nullptr; ++c)
-    {
-        if (++c->counter == c->limit) // Incrementa o contador e verifica se atingiu o limite.
-        {
-            jQueueSendFromISR(&jtaskQueue, &c->task); // Adiciona a tarefa na fila para ser executada posteriormente.
-            c->counter = 0; // Reinicia o contador.
+    for (uint8_t i = 0; i < jtaskIndex; i++) {
+        if (++jtaskStruct[i].counter >= jtaskStruct[i].limit) {
+            jQueueSendFromISR(&jtaskQueue, jtaskStruct[i].task);
+            jtaskStruct[i].counter = 0;
         }
     }
 }
 
-// Configuração inicial do timer
-// frequency: frequência em Hz
-// Exemplo: se frequency = 1000, a interrupção será chamada 1000 vezes por segundo (1 ms).
-bool jtaskSetup(uint32_t frequency)
-{
-    // Configuração do timer baseada na plataforma
+
+// Configuração do timer
+bool jtaskSetup(uint32_t frequency) {
     #ifdef ESP32
-        timer = timerBegin(frequency);                           // Inicializa o timer com a frequência desejada.
-        timerAttachInterrupt(timer, &timerCallback);             // Associa a função callback ao timer.
-        timerAlarm(timer, (80000000 / 80) / frequency, true, 0); // Configura o alarme do timer com base na frequência.
+    timer = timerBegin(0, 80, true); // Inicializa o timer com prescaler de 80
+    timerAttachInterrupt(timer, &timerCallback, true); // Configura a interrupção
+    timerAlarmWrite(timer, 80000000 / (80 * frequency), true); // Define a frequência
+    timerAlarmEnable(timer); // Ativa o alarme
     #else
-        cli(); // Desativa interrupções para configurar o timer de forma segura.
-        TCCR1A = 0; // Modo normal de operação.
-        TCCR1B = 0b00001011; // Configura o prescaler como 64.
-        OCR1A = (16000000 / (64 * frequency)) - 1; // Define o valor de comparação com base na frequência desejada.
-        TIMSK1 = 0b00000010; // Habilita a interrupção por comparação (OCIE1A).
-        sei(); // Habilita interrupções globais.
+    cli(); // Desativa interrupções
+
+    // Configurações para Timer 2
+    TCCR2A = 0b00000010; // Modo CTC (Clear Timer on Compare Match)
+    TCCR2B = 0b00000100; // Prescaler 64
+    OCR2A = (16000000 / (64 * frequency)) - 1; // Calcula o valor de comparação
+    TIMSK2 = 0b00000010; // Habilita interrupção por comparação A (OCIE2A)
+
+    sei(); // Habilita interrupções globais
     #endif
-
-    // Cria a fila para armazenar tarefas pendentes.
-    return jQueueCreate(&jtaskQueue);
+    return jQueueCreate(&jtaskQueue); // Cria a fila de tarefas
 }
 
-// Adiciona uma nova tarefa à estrutura de tarefas.
+// Anexa uma nova tarefa
 bool jtaskAttachFunc(void (*task)(), uint16_t limit) {
-    if (jtaskIndex >= NUMTASKS) { // Verifica se há espaço para adicionar mais tarefas.
-        return false; // Retorna false se o número máximo de tarefas for atingido.
-    }
-    jtaskStruct[jtaskIndex].counter = 0; // Inicializa o contador da tarefa.
-    jtaskStruct[jtaskIndex].limit = limit; // Define o limite da tarefa.
-    jtaskStruct[jtaskIndex].task = task; // Associa a função da tarefa.
-    jtaskIndex++; // Incrementa o índice para a próxima tarefa.
-    return true; // Retorna true indicando que a tarefa foi adicionada com sucesso.
+    if (jtaskIndex >= NUMTASKS) return false; // Verifica limite de tarefas
+    jtaskStruct[jtaskIndex] = {0, limit, task}; // Configura a tarefa
+    jtaskIndex++;
+    return true;
 }
 
-// Executa tarefas pendentes da fila.
-void jtaskLoop()
-{
-    volatile void (*taskMessage)(); // Ponteiro para armazenar a próxima tarefa a ser executada.
-    // Enquanto houver tarefas na fila, executa-as.
-    while (jQueueReceive(&jtaskQueue, &taskMessage) == true)
-    {
-        taskMessage(); // Executa a tarefa recebida da fila.
+// Executa as tarefas enfileiradas
+void jtaskLoop() {
+    void (*taskMessage)();
+    while (jQueueReceive(&jtaskQueue, &taskMessage)) {
+        taskMessage(); // Executa a tarefa
     }
 }
